3cbc77b3d5849608fcba2b24abb543d24e994588
Laurent Franceschetti <laurent.franceschetti@settlenext.com>
2021-09-09 09:03:20 +0200

    Bump version to 6.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages
 # Initialization
 # --------------------
 
-VERSION_NUMBER = '0.6.0'
+VERSION_NUMBER = '0.6.1'
 
 # required if you want to run document/test
 # pip install 'mkdocs-macros-plugin[test]'
